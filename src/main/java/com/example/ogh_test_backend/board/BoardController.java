75e89932cc9af60e572b74eb25990e0aed4d6580
gunha0405 <dhrjsgk0405@gmail.com>
2025-02-26 09:59:20 +0900

    [Feat] Board 상세 보기 기능 완성

--- src/main/java/com/example/ogh_test_backend/board/BoardController.java
+++ src/main/java/com/example/ogh_test_backend/board/BoardController.java
@@ -1,14 +1,12 @@
 package com.example.ogh_test_backend.board;
 
+import com.example.ogh_test_backend.board.model.dto.BoardReadDto;
 import com.example.ogh_test_backend.board.model.dto.BoardRegisterDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/board")
@@ -21,12 +19,26 @@ public class BoardController {
             description = "게시글을 등록 합니다.",
             responses = {
                     @ApiResponse(responseCode = "200", description = "조회 성공"),
-                    @ApiResponse(responseCode = "404", description = "사용자 없음")
+                    @ApiResponse(responseCode = "404", description = "작성 불가")
             }
     )
     @PostMapping("/register")
     public ResponseEntity<BoardRegisterDto.BoardRegisterRes> register(@RequestBody BoardRegisterDto.BoardRegisterReq dto) {
         BoardRegisterDto.BoardRegisterRes response = boardService.register(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "게시글 상세보기",
+            description = "주어진 ID에 해당하는 게시글을 조회 합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공"),
+                    @ApiResponse(responseCode = "404", description = "게시글 찾을 수 없음")
+            }
+    )
+    @GetMapping("/read/{boardIdx}")
+    public ResponseEntity<BoardReadDto.BoardReadRes> read(@PathVariable Long boardIdx) {
+        BoardReadDto.BoardReadRes response = boardService.read(boardIdx);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/ogh_test_backend/board/BoardService.java
+++ src/main/java/com/example/ogh_test_backend/board/BoardService.java
@@ -1,9 +1,12 @@
 package com.example.ogh_test_backend.board;
 
 import com.example.ogh_test_backend.board.model.Board;
+import com.example.ogh_test_backend.board.model.dto.BoardReadDto;
 import com.example.ogh_test_backend.board.model.dto.BoardRegisterDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,13 @@ public class BoardService {
     public BoardRegisterDto.BoardRegisterRes register(BoardRegisterDto.BoardRegisterReq dto) {
         Board board = boardRepository.save(dto.toEntity());
         return BoardRegisterDto.BoardRegisterRes.fromEntity(board);
+    }
+
+    public BoardReadDto.BoardReadRes read(Long boardIdx) {
+        Optional<Board> board = boardRepository.findById(boardIdx);
+        if (board.isPresent()) {
+            return BoardReadDto.BoardReadRes.fromBoard(board.get());
+        }
+        return null;
     }
 }
