b6cc524edbaea2717e2542243bdf05229481cad2
gunha0405 <dhrjsgk0405@gmail.com>
2025-02-26 10:26:18 +0900

    [Feat] Comment 기본 세팅 및 Board와 관계 설정

---
+++ src/main/java/com/example/ogh_test_backend/comment/CommentController.java
@@ -0,0 +1,12 @@
+package com.example.ogh_test_backend.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+}
